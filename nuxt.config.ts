c9e78e53c19c4e0175ae0fdd0f58398cb4c59206
Animus1385 <raspopov-re@ranepa.ru>
2024-11-05 01:45:59 +0300

    верстка

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,13 +6,13 @@ export default defineNuxtConfig({
         preference: 'system',
         fallback: 'light',
     },
-    ssr: true, // Включает серверный рендеринг. Если используете статическую генерацию, установите false.
+    ssr: true, // Включает серверный рендеринг. Для статической генерации установите false.
     target: 'static', // Используйте 'static' для статической генерации
     app: {
         baseURL: '/', // Базовый URL приложения
     },
     build: {
-        publicPath: '/dist/', // Путь к статическим ресурсам
+        publicPath: '/_nuxt/', // Путь к статическим ресурсам
     },
     css: ['@/assets/styles/styles.scss'],
     modules: ['@nuxt/devtools', '@nuxt/image', '@nuxt/ui', 'nuxt-swiper', '@nuxt/icon'],
