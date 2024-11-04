8352bb7e3d4644d4784c37b676f22532a55ec5c2
Animus1385 <raspopov-re@ranepa.ru>
2024-11-04 03:01:08 +0300

    верстка

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,15 +6,24 @@ export default defineNuxtConfig({
         preference: 'system',
         fallback: 'light',
     },
+    ssr: true,
+    nitro: {
+        preset: 'node-server',
+    },
     css: ['@/assets/styles/styles.scss'],
     modules: ['@nuxt/devtools', '@nuxt/image', '@nuxt/ui', 'nuxt-swiper', '@nuxt/icon'],
     runtimeConfig: {
         public: {
-            wordpressUrl: 'http://korpkonflikty.local/graphql',
+            wordpressUrl: 'http://korpkonflikty.ru/graphql',
         },
     },
     tailwindcss: {
         viewer: false,
+    },
+    image: {
+        provider: 'static',
+        staticFilename: '[publicPath]/images/[name]-[hash][ext]',
+        domains: [], // Пустой массив, если не используете внешние домены
     },
     icon: {
         customCollections: [
@@ -32,8 +41,5 @@ export default defineNuxtConfig({
                 },
             },
         },
-    },
-    routeRules: {
-        '/': { prerender: false },
     },
 });
