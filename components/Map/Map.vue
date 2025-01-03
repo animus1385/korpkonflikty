90d5bdada2b8d7026e8415ca401b852d0f34cc8c
Animus1385 <raspopov-re@ranepa.ru>
2025-01-03 22:29:05 +0300

    fix

--- components/Map/Map.vue
+++ components/Map/Map.vue
@@ -3,14 +3,21 @@
         <div class="map__container container">
             <div class="map__content">
                 <yandex-map v-model="map" :settings="{
+                    showScaleInCopyrights: true,
                     location: {
                         center: [props?.data.fields.centerX, props?.data.fields.centerY],
                         zoom: props?.data.fields.zoom,
+
                     },
                 }" width="100%" height="500px">
-                    <yandex-map-marker position="top-center left-center"
-                        :settings="{ coordinates: [props?.data.fields.metka.centerX, props?.data.fields.metka.centerY] }">
-                    </yandex-map-marker>
+                    <yandex-map-default-marker position="top-center left-center" :settings="{
+                        coordinates: [props?.data.fields.metka.centerX, props?.data.fields.metka.centerY],
+
+                        title: props?.data.fields.metka.title,
+                        subtitle: props?.data.fields.metka.descr,
+                        color: props?.data.fields.metka.color,
+                    }">
+                    </yandex-map-default-marker>
                     <yandex-map-default-scheme-layer />
                     <yandex-map-default-features-layer />
                 </yandex-map>
@@ -22,7 +29,12 @@
 <script setup lang="ts">
 const props = defineProps<{ data: any }>();
 import type { YMap } from '@yandex/ymaps3-types';
-import { YandexMap, YandexMapDefaultFeaturesLayer, YandexMapDefaultSchemeLayer, YandexMapMarker } from 'vue-yandex-maps';
+import {
+    YandexMap,
+    YandexMapDefaultFeaturesLayer,
+    YandexMapDefaultMarker,
+    YandexMapDefaultSchemeLayer,
+} from 'vue-yandex-maps';
 
 const map = shallowRef<null | YMap>(null);
 </script>
