832f420319e7a0903f7645540a67fd6776bee164
Animus1385 <raspopov-re@ranepa.ru>
2025-01-15 21:18:55 +0300

    fix

--- server/api/__sitemap__/urls.ts
+++ server/api/__sitemap__/urls.ts
@@ -1,6 +1,24 @@
 import type { SitemapUrlInput } from '#sitemap/types';
 
 export default defineSitemapEventHandler(async () => {
-    const { $api } = useNuxtApp();
-    return (await $api.getSiteMap()) satisfies SitemapUrlInput[];
+    const req = $fetch(process.env.PUBLIC_GRAPHQL_URL as string, {
+        method: 'POST',
+        body: {
+            query: 'query getPage {\n  allUrls\n}',
+        },
+    });
+    const departmentUrl = [];
+    const result = await req.then((e: any) => e.data.allUrls);
+    if (result) {
+        result
+            .filter((e: any) => e.includes('/blog'))
+            .every((e: string) => {
+                departmentUrl.push({
+                    loc: e,
+                    _sitemap: 'blog',
+                });
+            });
+    }
+
+    return result satisfies SitemapUrlInput[];
 });
