de7aade22bffbac0a873dd7fc5323ab0c979a03d
Animus1385 <raspopov-re@ranepa.ru>
2025-03-13 01:08:08 +0300

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,24 +17,28 @@
 const menuStore = useMenuStore();
 const storeCommon = useCommonStore();
 
-menuStore.getLayout();
+onMounted(() => {
+    menuStore.getLayout();
+});
 
-useSchemaOrg([
-    defineOrganization({
-        url: menuStore.siteUrl,
-        logo: "/favicon.ico",
-        name: menuStore.siteName,
-        address: {
-            "@type": "PostalAddress",
-            addressCountry: menuStore.footer.address.addressCountry,
-            postalCode: menuStore.footer.address.postalCode,
-            streetAddress: menuStore.footer.address.streetAddress,
-            addressLocality: menuStore.footer.address.addressLocality,
-        },
-        telephone: menuStore.footer.tel.title,
-        email: menuStore.footer.mail.title,
-    }),
-]);
+if (menuStore.siteUrl && menuStore.siteName) {
+    useSchemaOrg([
+        defineOrganization({
+            url: menuStore.siteUrl,
+            logo: "/favicon.ico",
+            name: menuStore.siteName,
+            address: {
+                "@type": "PostalAddress",
+                addressCountry: menuStore.footer.address.addressCountry,
+                postalCode: menuStore.footer.address.postalCode,
+                streetAddress: menuStore.footer.address.streetAddress,
+                addressLocality: menuStore.footer.address.addressLocality,
+            },
+            telephone: menuStore.footer.tel.title,
+            email: menuStore.footer.mail.title,
+        }),
+    ]);
+}
 </script>
 
 <style scoped></style>
