ef533d978d523c9c5155929abd6810964da550ed
Animus1385 <raspopov-re@ranepa.ru>
2025-03-18 21:41:18 +0300

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'korp-konflikty',
+            name: 'korp-konflikty.ru',
             port: '3000',
             exec_mode: 'cluster',
             instances: 'max',
