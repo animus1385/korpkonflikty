a07968921014435fd948be0fa10dfe55a05b7c89
Animus1385 <raspopov-re@ranepa.ru>
2025-03-31 10:09:20 +0300

    fix

--- pages/services/[slug].vue
+++ pages/services/[slug].vue
@@ -28,11 +28,10 @@
 							<meta itemprop="name" :content="elem.name" />
 						</div>
 
-						<div v-else class="breadcrumbs__item" itemprop="item" :itemid="elem.url">
+						<div v-else class="breadcrumbs__item" itemprop="item" :itemid="elem.url" :href='elem.url'>
 							<span>{{ elem.name }}</span>
 							<meta itemprop="position" :content="`${index + 1}`" />
 							<meta itemprop="name" :content="elem.name" />
-              <meta itemprop="id" :content="elem.url" />
 							<UIcon
 								v-if="index !== data.breadcrumbs.length - 1"
 								name="custom-icons:arrow-right-breadcrumbs"
